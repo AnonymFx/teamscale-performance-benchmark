4bc4220997c0bc34ea05bb8e222b4159b8a8b1a9
Thomas Pettinger <pettinger.thomas@gmail.com>
2021-09-08 16:39:28 +0200

    Adapt init script to work on Windows

--- init.sh
+++ init.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env sh
 python -m venv venv
+if [[ -d venv/bin ]]; then
 source venv/bin/activate
+elif [[ -d venv/Scripts ]]; then
+source venv/Scripts/activate
+else
+	echo "Activation script not found in venv, aborting..."
+	exit
+fi
 pip install -r requirements.txt
